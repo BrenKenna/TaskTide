cfb10f659194bfe7e12d0f44f41a23d3f0611b77
Brendan Kenna <brendan.kenna.bk@gmail.com>
2025-07-01 11:25:31 +0100

    GenericTreeNode
    Added untested methods for adding, dropping, and getting by address/value

--- tasktide/tasktide/src/main/java/org/tasktide/tasktide/parser/model/GenericTreeNode.java
+++ tasktide/tasktide/src/main/java/org/tasktide/tasktide/parser/model/GenericTreeNode.java
@@ -4,10 +4,13 @@
  */
 package org.tasktide.tasktide.parser.model;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -21,10 +24,10 @@ public class GenericTreeNode<T> {
     private GenericTreeNode<T> parent;
     private Map<String, GenericTreeNode<T>> children;
     
-    
     public GenericTreeNode(T data, String address) {
         this.data = data;
         this.address = address;
+        this.children = new HashMap<>();
     }
     
     public String getAddress() {
@@ -36,7 +39,7 @@ public class GenericTreeNode<T> {
     }
     
     public boolean isEdge() {
-        return this.children == null;
+        return this.children.isEmpty();
     }
     
     public boolean hasChild(String query) {
@@ -47,45 +50,198 @@ public class GenericTreeNode<T> {
         return this.children.keySet();
     }
     
+    public boolean removeChild(String key) {
+        if (children.containsKey(key)) {
+            children.remove(key);
+            return true;
+        }
+        return false;
+    }
+    
+    public void clear() {
+        children.clear();;
+    }
+    
+    
+    
     public Map<String, GenericTreeNode<T>> fetchChildren() {
         return this.children;
     }
-    
     
     public boolean isRoot() {
         return this.parent == null;
     }
     
+    public void setParent(GenericTreeNode<T> newParent) {
+        this.parent = newParent;
+    }
     
-    public void addChild(String path, String delim, String query, T data) {
-        // String temp = query.toString();
-        String[] addressArr = query.split(delim);
+    public void setPointerNode(String segmentKey, String path) {
+        GenericTreeNode<T> ptrNode = new GenericTreeNode<>(null, path);
+        this.children.putIfAbsent(segmentKey, ptrNode);
+        ptrNode.parent = this;
+    } 
+    
+    
+    public void addChild(String path, String delim, T data) {
+        this.recursiveAdd(path, delim, path, data);
+    }
+    
+    
+    public GenericTreeNode<T> findByAddress(String path, String delim) {
+        return this.recursiveGetAddressData(path, delim, path);
+    }
+    
+    public T getDataForAddress(String path, String delim) {
+        return this.findByAddress(path, delim).getData();
+    }
+    
+    
+    public Map<String, GenericTreeNode<T>> getChildrenAtAddress(String path, String delim) {
+        GenericTreeNode<T> node = this.findByAddress(path, delim);
+        return node != null ? node.children : null;
+    }
+    
+    
+    public Map<String, T> getChildrenDataAtAddress(String path, String delim) {
+        GenericTreeNode<T> node = this.findByAddress(path, delim);
+        if (node == null || node.children == null) return null;
         
+        Map<String, T> result = new HashMap<>();
+        for (Map.Entry<String, GenericTreeNode<T>> entry : node.children.entrySet()) {
+            result.put(entry.getKey(), entry.getValue().getData());
+        }
+        
+        return result;
+    }
+    
+    
+    public GenericTreeNode<T> findByData(T query) {
+        
+        // Base case
+        if ( this.data != null && this.data.equals(query)) {
+            return this;
+        }
+        
+        // Search depth-first
+        for ( GenericTreeNode<T> child : this.children.values() ) {
+            GenericTreeNode<T> result = child.findByData(query);
+            if ( result != null ) {
+                return result;
+            }
+        }
+        
+        // Otherwise null
+        return null;
+    }
+    
+    
+    public List<GenericTreeNode<T>> getPathToRoot() {
+        List<GenericTreeNode<T>> path = new ArrayList<>();
+        GenericTreeNode<T> current = this;
+        while (current != null) {
+            path.add(current);
+            current = current.parent;
+        }
+        Collections.reverse(path);
+        return path;
+    }
+    
+    
+    public boolean removeByAddress(String address, String delim) {
+        String[] addressArr = address.split(Pattern.quote(delim), 2);
+        
+        if ( addressArr.length == 1 ) {
+            return this.removeChild(addressArr[0]);
+        }
+        
+        // Recursive descent
+        GenericTreeNode<T> next = this.children.get(addressArr[0]);
+        if ( next != null ) {
+            return next.removeByAddress(addressArr[1], delim);
+        }
+        else {
+            return false;
+        }
+    }
+
+    
+    private GenericTreeNode<T> recursiveGetAddressData(String path, String delim, String query) {
+        String[] addressArr = query.split(delim, 2);
+
+        // Base case
+        if ( addressArr.length == 1 ) {
+            
+            // Return data if address matches
+            if ( this.address.equals(query) ) {
+                return this;
+            }
+            
+            // Otherwise null
+            return null;
+        }
+        
+        // Handle descending path
+        else {
+            
+            // Fetch next node
+            GenericTreeNode<T> next = this.children.get(addressArr[0]);
+            
+            // Return null if empty
+            if ( next == null ) {
+                return null;
+            }
+            
+            // Proceeding query child
+            else {
+                return next.recursiveGetAddressData(path, delim, addressArr[1]);
+            }
+        }
+    }
+    
+    
+    private void recursiveAdd(String path, String delim, String query, T data) {
+        
+        // Base case
+        String[] addressArr = query.split(delim, 2);
         if (addressArr.length == 1) {
             GenericTreeNode<T> node = new GenericTreeNode<>(data, path);
             node.parent = this;
             this.children.put(addressArr[0], node);
         }
         
-        String childPath = Arrays.asList(addressArr).subList(0, addressArr.length).toString();
-        GenericTreeNode<T> next = this.children.get(addressArr[1]);
-        
-        next.addChild(path, delim, childPath, data);
-    }
-    
-    
-    public T getChild(String delim, String query) {
-        String[] addressArr = query.split(delim);
-        
-        if (addressArr.length == 1) {
-            if ( this.address.equals(query) ) {
-                 return this.data;
+        // Descend down path via query
+        else {
+            String currentPath = this.subStringPathToFlag(path, addressArr[0], delim);
+            this.setPointerNode(addressArr[0], currentPath);
+            GenericTreeNode<T> next = this.children.get(addressArr[0]);
+            
+            if ( next != null ) {
+                next.recursiveAdd(path, delim, addressArr[1], data);
             }
         }
+    }
+
+    
+    
+    private String subStringPathToFlag(String path, String flag, String delim) {
+        String[] pathArr = path.split(Pattern.quote(delim));
+        StringBuilder result = new StringBuilder();
         
-        String childPath = Arrays.asList(addressArr).subList(0, addressArr.length).toString();
-        GenericTreeNode<T> next = this.children.get(addressArr[1]);
-        
-        return next.getChild(delim, childPath);
+        int i = 0;
+        while (i < pathArr.length && !pathArr[i].equals(flag)) {
+            if (i > 0) {
+                result.append(delim);
+            }
+            result.append(pathArr[i]);
+            i++;
+        }
+        if (i < pathArr.length) {
+            if (i > 0) {
+                result.append(delim);
+            }
+            result.append(pathArr[i]); // add the flag itself
+        }
+        return result.toString();
     }
 }
