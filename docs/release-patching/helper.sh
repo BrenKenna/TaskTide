337aa7004bb9b3fc64830b08d8463f4b6f8302f0
Brendan Kenna <55492113+BrenKenna@users.noreply.github.com>
2026-09-15 21:21:23 +0100

    Update helper.sh

--- docs/release-patching/helper.sh
+++ docs/release-patching/helper.sh
@@ -62,7 +62,7 @@ Successfully uploaded 1 asset to v0.9.0
 ##
 ## These note how the use-cases lead to developmental features
 ##
-## Candidates: Zip archive each
+## Candidates: Zip motivation of each reflects use-cases
 ## 239a7c2bf04ff0a0907b6bd3ad649aa9649f62d6, 26/02/19 <- Current
 ## e51273c4f82cff76535604b430a29e844d8d3853, 25/10/24 <- Leader Election
 ## 3f0b538d9bc5d97d2874b5494008443e999c9671, 25/08/18 <- Manager CQRS
@@ -75,7 +75,7 @@ Successfully uploaded 1 asset to v0.9.0
 REPO="https://github.com/BrenKenna/TaskTide.git"
 COMMIT="239a7c2bf04ff0a0907b6bd3ad649aa9649f62d6"
 DATE="2026-02-19"
-VERSION="v0.9.2"
+VERSION="v0.9.1-historical"
 
 
 # Configure active release
@@ -108,3 +108,16 @@ tar -czvf \
 # Clear repo dir
 cd ../../
 rm -fr $DATE
+
+
+# Upload artifacts
+cd $VERSION
+gh release upload $VERSION $(ls *gz)
+
+'''
+
+Successfully uploaded 3 assets to v0.9.1-historical
+
+Not worried about CI failure with worker hitting OOM, workflow is consistent
+https://github.com/BrenKenna/TaskTide/actions/runs/35018523089/job/104548299555
+'''
