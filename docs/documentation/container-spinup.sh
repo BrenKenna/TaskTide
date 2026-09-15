a066af15d39a0ec01adfafd6a5e665d2722ba924
Brendan Kenna <55492113+BrenKenna@users.noreply.github.com>
2026-09-15 22:46:04 +0100

    Bumps jersey bom
    Took earlier version bump for granted. State Summary<T, Integer> now failing to serialize. Will refactor to a string instead perhaps, or a mediating converter class.
    
    Suspected that this was fasterxml bump. while forgetting to bump this in api as well core lib, could mask that. bumping this and rolling back jersey bump worked fine. then re-bumping already yielded same. Retaining current jersey-bom 3.1.6
    
    Bumping jersey bom is a breaking change, requiring refactoring. No doubt for the better

--- docs/documentation/container-spinup.sh
+++ docs/documentation/container-spinup.sh
@@ -1,5 +1,7 @@
 
 # Starts MariaDB & couchDB containers for test
+docker container run --rm -e MARIADB_ROOT_PASSWORD=password -e MARIADB_DATABASE=tasktide -p 3306:3306 mariadb:11
+
 docker container run --rm \
     --name mariadb \
     --detach \
@@ -15,3 +17,7 @@ docker container run --rm \
     -e COUCHDB_PASSWORD=password \
     -p 5984:5984 \
     couchdb:3.5
+
+docker container run --rm -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -p 5984:5984 couchdb:3.5
+
+curl -u "admin:password" -X PUT http://localhost:5984/tasktide_database
