9e0208d3da865201ba915c2d4d3419cf1be3fbc0
Brendan Kenna <55492113+BrenKenna@users.noreply.github.com>
2026-09-15 21:49:00 +0100

    junit platform version

---
+++ docs/documentation/container-spinup.sh
@@ -0,0 +1,17 @@
+
+# Starts MariaDB & couchDB containers for test
+docker container run --rm \
+    --name mariadb \
+    --detach \
+    -e MARIADB_ROOT_PASSWORD=password \
+    -e MARIADB_DATABASE=tasktide \
+    -p 3306:3306 \
+    mariadb:11
+
+docker container run --rm \
+    --name couchdb \
+    --detach \
+    -e COUCHDB_USER=admin \
+    -e COUCHDB_PASSWORD=password \
+    -p 5984:5984 \
+    couchdb:3.5
